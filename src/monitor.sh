17164a6f266b36e4804b745fa28c877d3c83e6ab
Diego Retana <ludireta@hotmail.com>
2017-03-18 14:10:58 -0600

    Modificacion del script de monitoreo

--- src/monitor.sh
+++ src/monitor.sh
@@ -4,6 +4,7 @@
 #Programa de documentacion
 
 current_pwd=`pwd`
+current_pwd=$(pwd)
 
 cd /usr/share/doc
 echo Reporte: >> /tmp/reporte
@@ -11,4 +12,5 @@ date >> /tmp/reporte
 pwd >> /tmp/reporte
 du -sh . >> /tmp/reporte
 
+echo Path de ejecucion `pwd`
 cd $current_pwd
